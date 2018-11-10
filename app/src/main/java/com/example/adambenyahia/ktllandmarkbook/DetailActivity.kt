ee5bb266842f5a95a0226fea19d7e1ab8adc3783
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-11-11 00:34:49 +0100

    moving on

--- app/src/main/java/com/example/adambenyahia/ktllandmarkbook/DetailActivity.kt
+++ app/src/main/java/com/example/adambenyahia/ktllandmarkbook/DetailActivity.kt
@@ -1,2 +1,19 @@
 package com.example.adambenyahia.ktllandmarkbook
 
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail.*
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val intent = intent
+
+        val name = intent.getStringExtra("name")
+
+        textView.text = "name"
+    }
+}
--- app/src/main/java/com/example/adambenyahia/ktllandmarkbook/MainActivity.kt
+++ app/src/main/java/com/example/adambenyahia/ktllandmarkbook/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.example.adambenyahia.ktllandmarkbook
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var landmarkNames = ArrayList<String>()
+        landmarkNames.add("Big Ben")
+        landmarkNames.add("Empire State Building")
+        landmarkNames.add("Sainte Sophie")
+        landmarkNames.add("Tour de Pise")
+        landmarkNames.add("Tour Eiffel")
+
+        val bigben = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.bigben)
+        val empire = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.empirestatebuilding)
+        val saintsophie = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.saintesophie)
+        val pise = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.tourdepise)
+        val eiffel = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.toureiffel)
+
+        val landmarkImages = ArrayList<Bitmap>()
+
+        landmarkImages.add(bigben)
+        landmarkImages.add(empire)
+        landmarkImages.add(saintsophie)
+        landmarkImages.add(pise)
+        landmarkImages.add(eiffel)
+
+        val arrayAdapter =  ArrayAdapter(this,android.R.layout.simple_list_item_1,landmarkNames)
+
+        listView.adapter = arrayAdapter
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i,l ->
+
+            val intent = Intent(applicationContext,DetailActivity::class.java)
+            intent.putExtra("name",landmarkNames[i])
+
+            startActivity(intent)
+
+        }
+
     }
+
+
 }
